0242b27b84fd4a244f002bfc4daf4567528b90b5
IvanovNR <IvanovNR@310D-12>
2024-12-05 10:23:27 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        public static string Likes(string[] names)
+        {
+            int count = names.Length;
+
+            if (count == 0)
+                return "no on likes this";
+            else if (count == 1)
+                return $"{names[0]} likes this";
+            else if (count == 2)
+                return $"{names[0]} and {names[1]} likes this";
+            else if (count == 3)
+                return $"{names[0]} ans {names[1]} and {names[2]} likes this";
+            else 
+                return $"{names[0]}, {names[1]} and {count - 2} others like this";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Likes(new string[] {}));
+            Console.WriteLine(Likes(new string[] {"Peter"}));
+            Console.WriteLine(Likes(new string[] {"Peter", "John"}));
+            Console.WriteLine(Likes(new string[] {"Peter", "John", "Mary"}));
+            Console.WriteLine(Likes(new string[] {"Peter", "John", "Mary", "Alice"}));
+        }
+    }
+}
